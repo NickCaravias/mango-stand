dba72064d38cddb234b60aa2cd8e5a9ca0ce7f2b
NickCaravias <nick.caravias@gmail.com>
2025-01-05 23:49:17 -0500

    product box

---
+++ client/src/components/ProductBox.tsx
@@ -0,0 +1,12 @@
+// create a basic product component
+import React from 'react';
+
+const ProductBox = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold"></h1>
+    </div>
+  );
+};
+
+export default Products;
